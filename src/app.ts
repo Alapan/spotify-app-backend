9869de5cc669b9261db7bf6a6358b0bf9cebc786
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2023-04-23 17:13:24 +0300

    First config files and routes

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Request received.');
+});
+
+app.listen(PORT, () => {
+  return console.log(`Listening on port ${PORT}`);
+});
